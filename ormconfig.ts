8e95fd5752047f4470257fdb88b81d5847b84253
trevor-shepard <trevorbshepard@gmail.com>
2021-07-07 12:58:18 -0500

    transfer to postgres

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,15 @@
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-const config: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: '../db',
-  entities: ['dist/**/*.entity.js'],
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_NAME || 'trivvr_development_database',
   synchronize: true,
-  logging: false,
+  logging: true,
+  entities: [`dist/**/*.entity.js`],
 };
 
 export default config;
